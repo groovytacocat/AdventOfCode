8fb84b640b87f5a00bc968e840409353d522e912
groovytacocat <71411552+groovytacocat@users.noreply.github.com>
2023-12-04 20:43:01 -0500

    Day 4 pt 2/Cleaned up

---
+++ 2023/Day04/D4.py
@@ -0,0 +1,25 @@
+f = open('input.txt')
+windict = {}
+total = {}
+cardPoints = 0
+grandtotal = 0
+for line in f:
+    line = line[line.index('d')+1:-1]
+    cardNum = line[:line.index(':')].strip()
+    nums = line[line.index(':') + 1:].split('|')
+    wins = nums[0].strip().split()
+    picks = nums[1].strip().split()
+    winCount = 0
+    for vals in picks:
+        if vals in wins:
+            winCount += 1
+    if winCount > 0:
+        cardPoints += 2 ** (winCount - 1)
+    windict[int(cardNum)] = winCount
+    total[int(cardNum)] = 1
+f.close()
+for i in total:
+    for j in range(i+1, i+1+windict[i]):
+        total[j] += total[i]
+    grandtotal += total[i]
+print(f'Total Points: {cardPoints} | Total Cards: {grandtotal}')
